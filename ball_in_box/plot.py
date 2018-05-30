137cdfb78f830f771e75e3b4dbeb081ea5b82101
MrMofu <mr_mofu@163.com>
2018-05-30 12:11:20 +0800

    添加绘图功能-杜欣伟“

---
+++ ball_in_box/plot.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-  
+# 绘图工具 杜欣伟
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
+def answer(circles,blockers):    
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111)
+    for block in blockers:
+        ax.scatter(block[0],block[1], c='k')
+    for circle in circles:
+        x = circle[0]
+        y = circle[1]
+        r = circle[2]    
+        cir = Circle(xy = (x, y), radius=r, alpha=0.4)
+        ax.add_patch(cir)
+    plt.xlim(-1.0, 1.0)
+    plt.ylim(-1.0, 1.0)
+    plt.show()
